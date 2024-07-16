2d557477b9d71c1ff04cd76859ae348babf1167c
Darkmen203 <dima-pivovarov03@mail.ru>
2024-07-17 00:23:34 +0300

    fix loading adaptive

--- components/Loading.jsx
+++ components/Loading.jsx
@@ -1,16 +1,16 @@
 import React from 'react'
 
 export default function Loading() {
-    return (
-        <div className="relative w-36 h-36 rounded-full outline outline-1 outline-yellow-500">
-        <svg height="150" width="150" className="absolute top-0 left-0 transform -rotate-90">
-          <circle
-            cx="75"
-            cy="75"
-            r="65"
-            className="stroke-yellow-500 stroke-20 fill-none animate-bar-fill"
-          />
-        </svg>
-      </div>
-    )
+  return (
+    <div className="relative w-full h-full max-w-[150px] max-h-[150px] aspect-square rounded-full outline outline-1 outline-yellow-500">
+      <svg className="absolute inset-0 w-full h-full transform -rotate-90">
+        <circle
+          cx="50%"
+          cy="50%"
+          r="calc(50% - 10px)"
+          className="stroke-yellow-500 stroke-[10] fill-none animate-bar-fill"
+        />
+      </svg>
+    </div>
+  )
 }
